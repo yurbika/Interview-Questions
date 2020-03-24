e4961783862bac500d325e261182d712c7082b69
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-03-24 15:10:02 +0100

    adding navbar, adding home component

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./navbar.styles.scss";
+
+const Navbar = () => (
+  <div className="navbar">
+    <div>
+      <span>JavaScript</span>
+    </div>
+    <div>
+      <span>CSS</span>
+    </div>
+    <div>
+      <span>HTML</span>
+    </div>
+    <div>
+      <span>React</span>
+    </div>
+    <div>
+      <span>Redux</span>
+    </div>
+  </div>
+);
+
+export default Navbar;
---
+++ src/pages/home/home.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Navbar from "../../components/navbar/navbar.component";
+
+import "./home.styles.scss";
+
+const Home = () => (
+  <div className="container">
+    <Navbar />
+  </div>
+);
+
+export default Home;
