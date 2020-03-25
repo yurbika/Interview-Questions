dc24e8f214a06842d7944e44836c183df0a058dd
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-03-25 14:08:10 +0100

    adjusting home for new quester redux

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,23 +1,38 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 //components
 import Navbar from "../../components/navbar/navbar.component";
 import Quester from "../../components/quester/quester.component";
+
+//redux
+import { selectJavaScript } from "../../redux/home/home.selectors";
 
 //styles
 import "./home.styles.scss";
 
 class Home extends React.Component {
   render() {
+    const { jsVisible } = this.props;
+
+    //for more var
+    let show = false;
+    if (jsVisible) show = true;
+
     return (
       <div className="container">
         <div className="content-container">
           <Navbar />
-          <Quester />
+          {show ? <Quester /> : null}
         </div>
       </div>
     );
   }
 }
 
-export default Home;
+const mapStateToProps = createStructuredSelector({
+  jsVisible: selectJavaScript
+});
+
+export default connect(mapStateToProps)(Home);
