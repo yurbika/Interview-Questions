f5e040009ceecf1ff51e96adb187fdc9e4f459fb
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-03-26 14:35:44 +0100

    clear function in navigation implemented

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -18,48 +18,80 @@ import {
   setIndex,
   setInput,
   clearInput,
-  addToList
+  addToList,
+  clearEverything
 } from "../../redux/quester/quester.action";
 
-import { selectJavaScript } from "../../redux/home/home.selectors";
+import {
+  selectJavaScript,
+  selectSolution
+} from "../../redux/home/home.selectors";
 import toggle from "../../redux/home/home.action";
 import HomeActionTypes from "../../redux/home/home.types";
 
 //styles
 import "./navigation.styles.scss";
 
-const Navigation = ({ index, array, setIndex, addToList, toggle, input }) => {
+const Navigation = ({
+  index,
+  array,
+  setIndex,
+  addToList,
+  toggle,
+  input,
+  clearEverything,
+  clearInput,
+  solutionVisible
+}) => {
   return (
-    <div className="container">
-      {index > 0 ? (
+    <div className={"container" + (solutionVisible ? " solution" : "")}>
+      {!solutionVisible ? (
+        index > 0 ? (
+          <div className="button-wrapper">
+            <Button secondary onClick={() => setIndex(--index)}>
+              Back
+            </Button>
+          </div>
+        ) : null
+      ) : null}
+      <div className="button-wrapper">
+        <Button
+          onClick={() => {
+            /*
+        reseting everything
+        */
+            toggle(HomeActionTypes.SET_ALL_FALSE);
+            clearEverything();
+            if (!!document.getElementById("menu")) {
+              document.getElementById("menu").style.opacity = "1";
+              document.getElementById("menu").style.pointerEvents = "unset";
+            }
+          }}
+        >
+          Menu
+        </Button>
+      </div>
+      {!solutionVisible ? (
         <div className="button-wrapper">
-          <Button secondary onClick={() => setIndex(--index)}>
-            Back
+          <Button
+            secondary
+            onClick={() => {
+              if (index < array.length - 1) {
+                addToList({ [array[index]]: input });
+                setIndex(++index);
+              } else {
+                addToList({ [array[index]]: input });
+                toggle(HomeActionTypes.TOGGLE_JAVASCRIPT_QUESTIONS);
+                toggle(HomeActionTypes.TOGGLE_SOLUTION);
+              }
+              clearInput();
+            }}
+          >
+            {console.log(index)}
+            {index === array.length - 1 ? "Finish" : "Next"}
           </Button>
         </div>
       ) : null}
-      <div className="button-wrapper">
-        <Button>Menu</Button>
-      </div>
-      <div className="button-wrapper">
-        <Button
-          secondary
-          onClick={() => {
-            if (index < array.length - 1) {
-              addToList({ [array[index]]: input });
-              setIndex(++index);
-            } else {
-              addToList({ [array[index]]: input });
-              toggle(HomeActionTypes.TOGGLE_JAVASCRIPT_QUESTIONS);
-              toggle(HomeActionTypes.TOGGLE_SOLUTION);
-            }
-            clearInput();
-          }}
-        >
-          {console.log(index)}
-          {index === array.length - 1 ? "Finish" : "Next"}
-        </Button>
-      </div>
     </div>
   );
 };
@@ -69,7 +101,8 @@ const mapStateToProps = createStructuredSelector({
   array: selectQuestionsOrder,
   index: selectIndex,
   input: selectInput,
-  jsVisible: selectJavaScript
+  jsVisible: selectJavaScript,
+  solutionVisible: selectSolution
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -78,6 +111,7 @@ const mapDispatchToProps = dispatch => ({
   setInput: str => dispatch(setInput(str)),
   clearInput: () => dispatch(clearInput()),
   addToList: object => dispatch(addToList(object)),
+  clearEverything: () => dispatch(clearEverything()),
   //home action
   toggle: type => dispatch(toggle(type))
 });
